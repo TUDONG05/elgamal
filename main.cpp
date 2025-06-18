8d74ec2d3d6c8d5b998e69daa72d53adb53e5219
TUDONG05 <khactu731@gmail.com>
2025-06-19 00:07:18 +0700

    up

--- main.cpp
+++ main.cpp
@@ -2,142 +2,134 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long power(long long a, long long b, long long m)
+long long power(long long a, long long b, long long p)
 {
-    long long f=1;
-    a=a%m;
-    while(b>0)
+    long long f =1;
+    a =a%p;
+    while (b>0)
     {
-        if(b%2==1)
-            f = (f*a)%m;
-        a=(a*a)%m;
+        if(b&1)
+            f=(f*a)%p;
+        a = (a*a)%p;
         b=b/2;
-
-
     }
     return f;
 }
 
-//tim nghich dao cua a trong mod n
-long long nghich_dao(long long a, long long n)
+long long nghich(long long a, long long n)
 {
-    for(long long nd=2;nd<n;nd++)
-        if((a*nd)%n==1)
-            return nd;
+    for(int i=2;i<n;i++)
+        if((a*i)%n==1)
+            return i;
     return -1;
 }
-//ham ktra so nto
-bool isPrime(int n)
-{
-    for(int i=2;i<=sqrt(n);i++)
-        if(n%i==0)
-            return false;
-    return true;
-}
 
-//ham kiem tra phan tu sinh
-bool check_pts(int g, int p)
+bool check_snt(long long a)
 {
-    set <int > Set;
+    if(a<2)
+        return false;
+    else
+    {
+        for(int i=2;i<=sqrt(a);i++)
+            if(a%i==0)
+                return false;
+        return true;
+    }
+
+}
+bool check_pts(long long g, long long p)
+{
+    set <long long> Set;
     for(int i=1;i<p;i++)
         Set.insert(power(g,i,p));
-    return Set.size()==p-1;
-
+    return(Set.size()==p-1);
 }
+
 
 int main()
 {
-    long long p,g,x,k,m;
+    long long p;
 
-    do {
-        cout<<"nhap so nguyen to p>2:";
+    do
+    {
+        cout<<"Nhap so nguyen to lon p:";
         cin>>p;
     }
-    while(!isPrime(p)|| p<=2);
+    while(!check_snt(p));
 
+    long long g;
     do
     {
-        cout<<"nhap phan tu sinh g trong Z_"<<p<<":";
+        cout<<"Nhap ptu sinh g trong z_p:";
         cin>>g;
     }
-    while(g<=1 ||g>=p || !check_pts(g,p));
-
-    //nhap khoa bi mat
+    while(g<= 1 || g>=p || !check_pts(g,p));
+    long long x;
     do
     {
-        cout<<"Nhap khoa bi mat (1<x<p-1):";
+        cout<<"nhap khoa rieng x( 1<x<"<<p<<"):";
         cin>>x;
     }
-    while (x<=1 || x>=p-1);
-
-    //tinh khoa cog khai
+    while(x<=1 || x>=p);
     long long y = power(g,x,p);
-    cout<<"khoa cong khai y="<<y<<endl;
 
-    //nhap ban ro m
-   // do
-   // {
-    //    cout<<"nhap ban ro m(0<m<p):";
-   //     cin>>m;
-//
-  //  }
-   // while (m<=0 || m>=p);
-    //nhap k ngau nhien
-
-
-
+    long long k;
     do
     {
-        cout<<"Nhap so ngau nhien k (1<k<p-1):";
+        cout<<"nahp so ngau nhien k:";
         cin>>k;
+
     }
-    while (k<=1 || k>=p-1);
+    while(k<=1 || k>=p);
 
+    string s ;
+    cin.ignore();
+    cout<<"Nhap thong diep muon ma hoa:";
+    getline(cin,s);
 
-   string s;
-   cin.ignore();
-   cout<<"nhap thong diep:";
-   getline(cin,s);
+    //chuyen kitu -> so
+    vector <long long> m;
+    for(char si: s )
+        m.push_back(int(si));
 
-   vector <int > day;
-   for(char m:s)
-        day.push_back(int(m));
+    vector <pair<long long, long long>> banMo;
+    vector <long long> giaiMa;
 
-
-
-
-    vector<pair<long long , long long>> banMo;
-    vector<long long> banRo;
-
-    for(int m:day)
+    //ma hoa va giai ma
+    for(long long mi :m)
     {
-         //ma hoa
-    long long c1 = power(g,k,p);
-    long long c2 =(m*power(y,k,p))%p;
+        long long c1 = power(g,k,p);
+        long long c2 = (mi*power(y,k,p))%p;
 
-    banMo.push_back({c1,c2});
+        banMo.push_back({c1,c2});
 
-    //giai ma
-    long long s = power(c1,x,p);
-    long long s_1=nghich_dao(s,p);
-    if(s_1==-1)
-    {
-        cout<<"Ko tim duoc nghich dao cua s. Giai ma that bai!"<<endl;
-        return 1;
+        //giai ma
+        long long s = power(c1,x,p);
+        long long s_1 =nghich(s,p);
+
+        if(s_1 ==-1)
+        {
+            cout<<"ko tim dc nghich dao s_1, ko the giai ma!"<<endl;
+
+        }
+        else
+        {
+            long long mess = (c2*s_1)%p;
+            giaiMa.push_back(mess);
+
+
+        }
+
     }
+            cout<<"cac cap khoa (c1,c2):"<<endl;
+            for(auto &c :banMo)
+                cout<<"("<<c.first<<","<<c.second<<")";
+                cout<<endl;
 
-    long long giaiMa = (c2*s_1)%p;
-    banRo.push_back(giaiMa);
-    }
-    cout << "\nCac cap (c1, c2) sau khi ma hoa:\n";
-    for (auto &c : banMo)
-    cout << "(" << c.first << ", " << c.second << ") ";
-    cout << endl;
-
-    cout << "\nBan ro sau khi giai ma:\n";
-    for (long long val : banRo)
-    cout << char(val);
-    cout << endl;
+            cout<<"giai ma thong diep ta duoc:"<<endl;
+            for(long long i:giaiMa)
+                cout<<(char)i;
+            cout<<endl;
 
 
 }
